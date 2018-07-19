6fa9e1c0d2c53fbd76854a9d87dc39ae827b427e
Edwin Gawron <edwin_g@viper.local>
2018-07-19 12:56:33 +0200

    Revert to 20d9f389c426c13f5dd11e364aa426f1a39b7b72

--- assets/js/script.js
+++ assets/js/script.js
@@ -203,25 +203,3 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
 
 // execute above function
 initPhotoSwipeFromDOM('.lightbox-gallery');
-
-// Minifying
-// scripts.each do |script|
-//
-//        # Skip already minified files
-//        if script.path =~ /.min.js$/
-//            next
-//        else
-//            dest_path = script.path.sub! ".js", ".min.js"
-//            script_mtime = File.mtime(script.path)
-//
-//            # Only minify changed files
-//            unless File.exist?(dest_path) and File.mtime(dest_path) > script_mtime
-//                Jekyll.logger.debug "Minifying: %s" % script.path
-//                output = uglifier.compress(File.read(script.path))
-//
-//                File.open(dest_path, 'w') do |f|
-//                    f.write(output)
-//                end
-//            end
-//        end
-//    end
